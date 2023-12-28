c2e3acde72305875de74a89c86bb0becf75b8c3e
DavaDN <ayujas321@gmail.com>
2023-12-28 14:08:50 +0700

    first commit

---
+++ boot.js
@@ -0,0 +1,33 @@
+data = [{
+    'img1': 'asset/screenshot (92).png',
+    'img2': 'asset/screenshot (92).png',
+    'title': 'Web E-Commerce',
+    'subtitle': 'Html',
+    'desc' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aliquam, officia veniam nulla voluptates iusto.'
+},{
+    'img1': 'asset/screenshot (91).png',
+    'img2': 'asset/screenshot (91).png',
+    'title': 'Web E-Commerce',
+    'subtitle': 'CSS',
+    'desc' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aliquam, officia veniam nulla voluptates iusto.'
+}]
+
+var container = document.querySelector('.container-project')
+
+data.forEach(element => {
+    container.innerHTML +=
+    `<div class="project">
+    <div class="img-project">
+        <img src="`+element.img1+`" alt="">
+        <img src="${element.img2}" alt="">
+    </div>
+    <div class="desc-project">
+        <h1>${element.title}</h1>
+        <h2>${element.subtitle}</h2>
+        <h3>${element.desc}</h3>
+    </div>
+    <button class="read-more">read more</button>
+</div>`
+
+
+});
