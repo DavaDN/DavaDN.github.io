90c27cb1d0579deb89e320fc028a092954e8f673
DavaDN <155049573+DavaDN@users.noreply.github.com>
2024-01-25 10:00:05 +0700

    Add files via upload

---
+++ Gambar/Bootcamp/jawa.js
@@ -0,0 +1,33 @@
+data = [{
+    'img1': 'asset/beranda.png',
+    'img2': 'asset/beranda.png',
+    'title': 'Web E-Commerce',
+    'subtitle': 'Html',
+    'desc' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aliquam, officia veniam nulla voluptates iusto.'
+},{
+    'img1': 'asset/beranda2.png',
+    'img2': 'asset/beranda2.png',
+    'title': 'Web E-Commerce',
+    'subtitle': 'CSS',
+    'desc' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aliquam, officia veniam nulla voluptates iusto.'
+}]
+
+var container = document.querySelector('.container-project')
+
+data.forEach(element => {
+    container.innerHTML +=
+    `<div class="project">
+    <div class="img-project">
+        <img src="`+element.img1+`" alt="">
+        <img src="${element.img2}" alt="">
+    </div>
+    <div class="desc-project">
+        <h1>${element.title}</h1>
+        <h2>${element.subtitle}</h2>
+        <h3>${element.desc}</h3>
+    </div>
+    <button class="read-more">read more</button>
+</div>`
+
+
+});
